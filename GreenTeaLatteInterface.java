b7d622074d11a96170512fd119914cf7c68d43f1
David Castner <davidjcastner@gmail.com>
2017-01-26 01:01:48 -0500

    fix interface

--- GreenTeaLatteInterface.java
+++ GreenTeaLatteInterface.java
@@ -4,7 +4,7 @@ package greentealatte;
 public interface GreenTeaLatteInterface {
     // GreenTeaLatteInterface is an inferace that implements a behavior driven development testing framework
 
-    void describe(String description, Method testsToBeRun);
+    void describe(String description, Runnable testsToBeRun);
     // creates a category in a tree structure, so a category can be created inside of another category
     // places tests inside of a category
 
@@ -12,8 +12,8 @@ public interface GreenTeaLatteInterface {
     // pending test, test not yet implemented
     // results in a warning, but not a failed test
 
-    void it(Method testToRun);
-    void it(String descriptionOfFunctionality, Methods testToRun);
+    void it(Runnable testToRun);
+    void it(String descriptionOfFunctionality, Runnable testToRun);
     // creates a test to run
     // call inside of a category
 
@@ -22,24 +22,24 @@ public interface GreenTeaLatteInterface {
     // takes the boolean value of the result of a test and applies it to the test
     // call inside of a test
 
-    void before(Method setup);
-    void before(String description, Method setup);
-    // runs the setup method once before the category's tests begin
+    void before(Runnable setup);
+    void before(String description, Runnable setup);
+    // runs the setup runnable once before the category's tests begin
     // all hooks are run  in the order that defined
 
-    void after(Method cleanup);
-    void after(String description, Method cleanup);
-    // runs the cleanup method once after the category's tests complete
+    void after(Runnable cleanup);
+    void after(String description, Runnable cleanup);
+    // runs the cleanup runnable once after the category's tests complete
     // all hooks are run  in the order that defined
 
-    void beforeEach(Method setup);
-    void beforeEach(String description, Method setup);
-    // runs the setup method before each test in the category
+    void beforeEach(Runnable setup);
+    void beforeEach(String description, Runnable setup);
+    // runs the setup runnable before each test in the category
     // all hooks are run  in the order that defined
 
-    void afterEach(Method cleanup);
-    void afterEach(String description, Method cleanup);
-    // runs the cleanup method after each test in the category
+    void afterEach(Runnable cleanup);
+    void afterEach(String description, Runnable cleanup);
+    // runs the cleanup runnable after each test in the category
 
     void run();
     // runs all tests after the tests have been defined
